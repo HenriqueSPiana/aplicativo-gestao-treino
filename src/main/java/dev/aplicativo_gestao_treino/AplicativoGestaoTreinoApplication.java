0304ee694829ecc811dd7b777b96232deaa23d48
Henrique Schrock Piana <103382089+HenriqueSPiana@users.noreply.github.com>
2026-03-04 19:51:53 -0300

    inicia projeto spring

---
+++ src/main/java/dev/aplicativo_gestao_treino/AplicativoGestaoTreinoApplication.java
@@ -0,0 +1,13 @@
+package dev.aplicativo_gestao_treino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicativoGestaoTreinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicativoGestaoTreinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/aplicativo_gestao_treino/AplicativoGestaoTreinoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aplicativo_gestao_treino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicativoGestaoTreinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
