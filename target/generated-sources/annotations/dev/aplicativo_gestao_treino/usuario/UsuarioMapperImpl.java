e922ddd2b6d6928fcda3286ffba9178bb3427624
HenriqueSPiana <103382089+HenriqueSPiana@users.noreply.github.com>
2026-03-11 23:31:38 -0300

    modifica versionamento de banco para liquibase

---
+++ target/generated-sources/annotations/dev/aplicativo_gestao_treino/usuario/UsuarioMapperImpl.java
@@ -0,0 +1,35 @@
+package dev.aplicativo_gestao_treino.usuario;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2026-03-11T22:46:33-0300",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 25.0.2 (Oracle Corporation)"
+)
+@Component
+public class UsuarioMapperImpl implements UsuarioMapper {
+
+    @Override
+    public UsuarioDTO toDTO(UsuarioModel usuarioModel) {
+        if ( usuarioModel == null ) {
+            return null;
+        }
+
+        UsuarioDTO usuarioDTO = new UsuarioDTO();
+
+        return usuarioDTO;
+    }
+
+    @Override
+    public UsuarioModel toModel(UsuarioDTO usuarioDTO) {
+        if ( usuarioDTO == null ) {
+            return null;
+        }
+
+        UsuarioModel usuarioModel = new UsuarioModel();
+
+        return usuarioModel;
+    }
+}
